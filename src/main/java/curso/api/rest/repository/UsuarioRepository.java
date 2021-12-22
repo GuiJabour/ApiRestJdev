eea264212df89cd8b64e8a31ae8bf518bff3a2e2
Guilherme Henrique Chaves Jabour <henrique.jabour@gmail.com>
2021-12-22 13:51:36 -0300

    commit da app

---
+++ src/main/java/curso/api/rest/repository/UsuarioRepository.java
@@ -0,0 +1,15 @@
+package curso.api.rest.repository;
+
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import curso.api.rest.model.Usuario;
+
+@Repository
+public interface UsuarioRepository extends CrudRepository<Usuario, Long>  {
+
+	
+	
+	
+}
